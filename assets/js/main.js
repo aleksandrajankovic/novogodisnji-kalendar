9867121f022c099ebc3fe5e4b72db3db79719870
aleksandrajankovic <aleksandra.bgd.87@gmail.com>
2023-12-12 12:06:19 +0100

    Added new footer design

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,19 +142,73 @@ var fileName = url.substring(url.lastIndexOf('/')+1);
 
 $('a[href="' + fileName + '"]').parent().addClass('active');
 
-// function togglePopup() {
-//   var popup = document.querySelector('#modal');
+/*SWIPER FOOTER*/
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 15,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 3.5,
+      spaceBetween: 10,
+    },
+    480: {
+      slidesPerView: 3.5,
+      spaceBetween: 10,
+    },
 
-//   var isHidden = popup.classList.contains('hidden');
+    640: {
+      slidesPerView: 3.5,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4.5,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 8.5,
+      spaceBetween: 10,
+    },
+  },
+});
 
-//   if (isHidden) {
-//       popup.classList.remove('hidden');
-//   } else {
-//       popup.classList.add('hidden');
-//       location.reload();
-      
-//   }
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: 15,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 4.5,
+      spaceBetween: 10,
+    },
+    480: {
+      slidesPerView: 4.5,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 4.5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4.5,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 6.5,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 12.5,
+      spaceBetween: 10,
+    },
+  },
+});
 
-// }
 
